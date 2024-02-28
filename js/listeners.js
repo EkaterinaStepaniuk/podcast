3d89426599d038f2a01bb7971b2328e5dfe6eeaf
Ekaterina <ekaterina76542@gmail.com>
2024-02-28 23:10:01 +0300

    podcast

---
+++ js/listeners.js
@@ -0,0 +1,6 @@
+$(document).ready(function (){
+    $("slider").owlCarousel({
+        dots: true,
+        margin: 200
+    });
+});
---
+++ js/podcast-header.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+    $('.owl-carousel').owlCarousel({
+        dots: false,
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        smartSpeed: 700,
+        mouseDrag: false
+    });
+});
